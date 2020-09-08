a5ee400a383134b47ca5eeb86ba8a9e42819bca4
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-09-08 20:53:56 +0000

    learn to console.log on keyup for strategy to count text

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,10 +7,27 @@ $(document).ready(function() {
 
 
 
+// #tweet-text
 
+// change event
+// keydown event
+// keyup event
+// blur event
+// keypress event
 
+  // $('#tweet-text').change(() => { console.log('change event') });
 
+  $('#tweet-text').on('keyup', function(e) {
+    console.log($("#tweet-text").val().length)
+    console.log(this); 
+  })
 
+  // $('#tweet-text').blur(() => { console.log('blur event') })
+  
+  // $('#tweet-text').keyup(() => { console.log('keyup event') })
+  
+  // $('#tweet-text').keydown(() => { console.log('keydown event') })
 
-
+  // $('#tweet-text').keypress(() => { console.log('keypress event') })
+  
 });
