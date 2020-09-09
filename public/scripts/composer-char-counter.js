b99a23ff069b8967961af5c10fd0b7e9ea2672d6
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-09-09 00:45:48 +0000

    tinker with the styles of css to get everything to flow otgether. IMplement new section for tweet history on page by using html and css. Not yet dynamic

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,30 +1,27 @@
 //responsible for character counter of tweet input form
 
-$(document).ready(function() {
- 
+$(document).ready(function () {
+
   $('#tweet-text').on('keyup', function (e) {
     //tracking character count input from user using keyup
     const currCharCount = $(this).val().length
-    
+
     //locating the counter element
     const counterElem = $(this).parent().children('.under-tweet-input').children('.counter')
-      
-    //represents how many characters are available 
-    // const remainingChars = counterElem.val(); 
 
     const remainingChars = 140 - currCharCount;
     console.log(remainingChars)
 
     //updating counter elem on DOM to reflect current changes in remaining characters
-    counterElem.text(remainingChars); 
+    counterElem.text(remainingChars);
 
     if (remainingChars < 0) {
-      counterElem.css('color', 'red'); 
+      counterElem.css('color', 'red');
     } else {
-      counterElem.css('color', '#545149'); 
+      counterElem.css('color', '#545149');
     }
   })
 
 
-  
+
 });
