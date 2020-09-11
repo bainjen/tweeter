2025e1b7626a7260cc4820f7dd8105d4b212098d
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-09-11 01:12:03 +0000

    clean up code spacing, indentation, comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,40 +1,28 @@
-
 //function to build html for tweets dynamically
 const createTweetElement = function (tweetObj) {
- 
   const $tweet = $("<article class='tweet'>");
-
   //tweet header
   const $header = $("<header class='th-header'>");
-
   //header children
   const $headerD1 = $("<div class='name-left'>");
   const $avatar = $("<img>").attr("src", tweetObj.user.avatars);
   const $name = $("<h3>").text(tweetObj.user.name);
-
   $headerD1.append($avatar, $name);
-
   const $headerD2 = $("<div id='userID'>");
   const $handle = $("<p>").text(tweetObj.user.handle);
-
   $headerD2.append($handle);
   //append the contents of header
   $header.append($headerD1, $headerD2);
-
   //tweet content
   const $contentContainer = $("<div class='display-tweet'>");
-
   const $contentText = $("<p>").text(tweetObj.content.text);
   //append tweet text to container
   $contentContainer.append($contentText);
-
   //tweet footer
   const $footer = $("<footer>")
-  
   //tweet footer children 
-  const timeDelta =  moment(tweetObj.created_at).fromNow(); 
+  const timeDelta = moment(tweetObj.created_at).fromNow();
   const $timeStamp = $("<p>").text(timeDelta);;
-
   const $footerD2 = $("<div class='icons'>");
   const $flag = $("<i class='far fa-flag' id='flag'>");
   const $retweet = $("<i class='fas fa-retweet' id='retweet'>");
@@ -42,10 +30,8 @@ const createTweetElement = function (tweetObj) {
   $footerD2.append($flag, $retweet, $heart);
   //append footer children to footer
   $footer.append($timeStamp, $footerD2);
-
   //add all of the children to the tweet div
   $tweet.append($header, $contentContainer, $footer);
-
   return $tweet;
 };
 
@@ -64,7 +50,6 @@ const loadTweets = () => {
     method: 'GET',
     dataType: 'json',
     success: (tweets) => {
-      console.log(tweets);
       renderTweets(tweets);
     },
     error: (error) => {
@@ -75,7 +60,6 @@ const loadTweets = () => {
 
 const appendError = (message) => {
   $('#submit-tweet').prepend($("<span class='error'>").text(' ⚠️ ' + message + ' ⚠️').slideDown().delay(3500).hide(500));
-    // .slideDown(100).show(5000)
 }
 
 const removeError = () => {
@@ -89,22 +73,19 @@ $(document).ready(function () {
   $submitTweet.on('submit', function (e) {
     e.preventDefault();
     const serializedData = $(this).serialize();
-  
+    //handle errors
     removeError();
     if ($('#tweet-text').val() === '' || null) {
       appendError("You're a goose! 🐙 Type something! Anything!");
     } else if ($('#tweet-text').val().length > 140) {
       appendError("Ope! Too many characters 🙈 Love that enthusiasm! 💙 Just keep it under 140!")
     } else {
-
+      //post tweets
       $.post('/tweets', serializedData)
         .then((response) => {
-          // console.log("response is: ", response); 
           loadTweets();
           $(this).children('textarea').val('');
         })
     }
-
   });
-
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,27 +1,18 @@
 //responsible for character counter of tweet input form
-
 $(document).ready(function () {
-
   $('#tweet-text').on('keyup', function (e) {
     //tracking character count input from user using keyup
     const currCharCount = $(this).val().length
-
     //locating the counter element
     const counterElem = $(this).parent().children('.under-tweet-input').children('.counter')
-
     const remainingChars = 140 - currCharCount;
     console.log(remainingChars)
-
     //updating counter elem on DOM to reflect current changes in remaining characters
     counterElem.text(remainingChars);
-
     if (remainingChars < 0) {
       counterElem.css('color', 'red');
     } else {
       counterElem.css('color', '#545149');
     }
   })
-
-
-
 });
